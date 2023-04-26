8e31aa63783998293f44bf26e2906c3db586e411
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-04-26 19:12:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Nizar Rahman'// yourname
+global.MONGO_DB_URI = "mongodb+srv://naistore:<password>@cluster0.k9w0k4c.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "rnizar025" //email
+global.email_password = "cjmnadcwwreitwpm" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
